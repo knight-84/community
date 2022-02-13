df4ceeba128221553c8c21219f2d8dd019d2eaac
knight84 <k7678922@gmail.com>
2022-02-13 18:13:43 +0800

    首次提交

---
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -0,0 +1,101 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.AlphaDao;
+import com.nowcoder.community.service.AlphaServcie;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class CommunityApplicationTests implements ApplicationContextAware {
+
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	@Test
+	public  void testApplicationContext() {
+		System.out.println(applicationContext);
+
+		AlphaDao alphaDao = applicationContext.getBean(AlphaDao.class);
+		System.out.println(alphaDao.select());
+
+		alphaDao = applicationContext.getBean("alphaHibernate", AlphaDao.class);
+		System.out.println(alphaDao.select());
+	}
+
+	@Test
+	public  void testBeanManagement() {
+		AlphaServcie alphaServcie = applicationContext.getBean(AlphaServcie.class);
+		System.out.println(alphaServcie);
+
+		alphaServcie = applicationContext.getBean(AlphaServcie.class);
+		System.out.println(alphaServcie);
+	}
+
+	@Test
+	public  void testBeanConfig() {
+		SimpleDateFormat simpleDateFormat =
+				applicationContext.getBean(SimpleDateFormat.class);
+		System.out.println(simpleDateFormat.format(new Date()));
+	}
+
+	@Autowired
+	@Qualifier("alphaHibernate")
+	private AlphaDao alphaDao;
+
+	@Autowired
+	private AlphaServcie alphaServcie;
+
+	@Autowired
+	private SimpleDateFormat simpleDateFormat;
+
+	@Test
+	public void testDI() {
+		System.out.println(alphaDao);
+		System.out.println(alphaServcie);
+		System.out.println(simpleDateFormat);
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
