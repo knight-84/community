ffca154a28a235ad105438a0dee0bfda2ccffc82
knight84 <k7678922@gmail.com>
2022-02-13 21:43:06 +0800

    second commit

---
+++ src/test/java/com/nowcoder/community/GitTest.java
@@ -0,0 +1,12 @@
+package com.nowcoder.community;
+
+import org.junit.Test;
+
+public class GitTest {
+
+    @Test
+    public void testGit() {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+    }
+}
